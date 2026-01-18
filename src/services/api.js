b70f0829471c0842cc2b4a882b5881df92469718
yashatratechnologys <yashatratechnologys@gmail.com>
2026-01-18 14:26:01 +0530

    API UPDATED

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://www.yashastratechnologies.com/api",
+  baseURL: "https://yashastra-backend.onrender.com/api",
 });
 
 export default API;
